75bff9d4c526ee67b50e31bc6f7ce69be4d4e763
vlad <vl.hutsal@gmail.com>
2020-12-18 21:24:55 +0200

    update: before runing each test, Cypress chose first message of inbox (as @firstMsg) add: test to check if the picture exists and has non empty attributes add: test to check if there are span descendants of media-body div in @firstMsg

--- cypress/integration/inbox_pageTest.js
+++ cypress/integration/inbox_pageTest.js
@@ -1,8 +1,29 @@
 describe('Inbox page tests', () => {
-    before(() => {
-        cy.login()
+  before(() => {
+    cy.login();
+  })
+
+  beforeEach(() => {
+    cy.get('h3').next().children('.inbox-message').first()
+      .as('firstMsg');
+  }) 
+
+  it('checks if picture exists', () => {
+    cy.get('@firstMsg').within(() => {
+      cy.get('.media > .media-left > user-picture')
+      cy.get('[picture=""]')
+        .should('not.exist')
+
+      cy.get('[name=""]')
+        .should('not.exist')     
+    });
+  })
+
+  it('checks if recruiters name and position exists', () => {
+    cy.get('@firstMsg').within(() => {
+      cy.get('.media-body').then(($el) => {
+        expect($el).to.have.descendants('span')
+      })
     })
-    it('says hello', () => {
-        cy.log('hello!')
-    })
+  })
 })
