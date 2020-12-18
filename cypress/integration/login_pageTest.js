c28bd1f8591d951654fcaab2bd14738c42a75539
vlad <vl.hutsal@gmail.com>
2020-12-18 19:41:39 +0200

    add: test to check if there are valid a social network buttons on login page add: test to ceck if there are Register and Forgot password? links add: test to login through UI

---
+++ cypress/integration/login_pageTest.js
@@ -0,0 +1,30 @@
+describe('Login page tests', () => {
+  before(() => {
+    cy.visit(Cypress.env('LoginPage'))
+  })
+
+  it('checks if there are social buttons', () => {
+    cy.get('.col-social-login')
+      .children()
+      .each(button => {
+        cy.wrap(button)
+          .children()
+          .should('have.attr', 'name', 'social_auth')
+      })
+  })
+
+  it('checks Register and Remember password buttons', () => {
+    cy.contains('Реєстрація')
+      .should('have.attr', 'href', '/signup')
+
+    cy.contains('Забули пароль?')
+      .should('have.attr', 'href', '/remind?email=&lang=uk')
+  })
+
+  it('checks login', () => {
+    cy.get('#email').type('vl.hutsal@gmail.com')
+    cy.get('#password').type('easytobreakinparol{enter}')
+    cy.url().should('eq', Cypress.env('InboxPage'))
+  })
+
+})
