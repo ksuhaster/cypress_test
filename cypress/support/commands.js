a29076484cfb0c5eff9fb2c506157511e6808561
vlad <vl.hutsal@gmail.com>
2020-12-19 23:59:49 +0200

    add: getResponse command, to check if url is valid

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,24 @@
 Cypress.Commands.add('login', () => {
-    cy.visit(Cypress.env('LoginPage'))
-    cy.get('#email').type('vl.hutsal@gmail.com')
-    cy.get('#password').type('easytobreakinparol{enter}')
-    cy.url().should('eq', Cypress.env('InboxPage'))
+  cy.visit(Cypress.env('LoginPage'))
+  cy.get('#email').type('vl.hutsal@gmail.com')
+  cy.get('#password').type('easytobreakinparol{enter}')
+  cy.url().should('eq', Cypress.env('InboxPage'))
 })
 
+
 Cypress.Commands.add('getFirstMsg', () => {
-    cy.get('h3').next().children('.inbox-message').first();
+  cy.get('h3').next().children('.inbox-message').first();
+})
+
+
+Cypress.Commands.add('getResponse', (url, expectedStatus, expectedUrl) => {
+  cy.request({
+    url: url,
+    followRedirect: false
+  })
+    .then((response) => {
+      console.log(expectedUrl)
+      expect(response.status).to.eq(expectedStatus)
+      expect(response.allRequestResponses[0]['Request URL']).to.eq(expectedUrl)
+    });
 })
