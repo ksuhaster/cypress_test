8c3c83fc4f38dbefd74780b3fbf533c8b17d91a4
vlad <vl.hutsal@gmail.com>
2020-12-22 01:32:40 +0200

    add: validateNavElements commands update: validateUrlResponse now works without status as an argument

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,19 +6,25 @@ Cypress.Commands.add('login', () => {
 })
 
 
+Cypress.Commands.add('logout', () => {
+  cy.visit('https://djinni.co/logout');
+})
+
+
 Cypress.Commands.add('getFirstMsg', () => {
   cy.get('h3').next().children('.inbox-message').first();
 })
 
 
-Cypress.Commands.add('validateUrlResponse', (requestUrl, expectedStatus, expectedRespUrl) => {
+Cypress.Commands.add('validateUrlResponse', (requestUrl, expectedRespUrl) => {
   const pattern = new RegExp(expectedRespUrl);
+  const expStatuses = [302, 200]
   cy.request({
     url: requestUrl,
     followRedirect: false
   })
     .then((response) => {
-      expect(response.status).to.eq(expectedStatus)
+      expect(expStatuses).to.include(response.status)
       expect(response.allRequestResponses[0]['Request URL']).to.match(pattern)
     });
 })
@@ -27,4 +33,19 @@ Cypress.Commands.add('validateUrlResponse', (requestUrl, expectedStatus, expecte
 Cypress.Commands.add('checkPictureNotEmpty', () => {
   cy.get('user-picture[picture=""]').should('not.exist');
   cy.get('user-picture[name=""]').should('not.exist');
+})
+
+
+Cypress.Commands.add('validateNavElments', (Ul, expLinks) => {
+  cy.wrap(Ul)
+    .children('li')
+    .each((li, idx) => {
+      cy.wrap(li).within(() => {
+        cy.get('a')
+          .invoke('attr', 'href')
+          .then(expUrl => {
+            cy.validateUrlResponse(expUrl, expLinks[idx])
+          });
+      });
+    });
 })
