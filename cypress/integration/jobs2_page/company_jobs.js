d0e8915a0964ec09fddc489ee3f30f3c1d0d56da
vlad <vl.hutsal@gmail.com>
2020-12-22 17:52:26 +0200

    update: now validates without crossing other pages update: checks header and company name of each job of the list. comparing with env variable

--- cypress/integration/jobs2_page/company_jobs.js
+++ cypress/integration/jobs2_page/company_jobs.js
@@ -1,65 +1,33 @@
-describe('Test company open jobs page [Company jobs page]', () => {
+describe('[Company jobs page] Tests if link ', () => {
   before(() => {
-    cy.login();
-    cy.visit('https://djinni.co/r/67295-recruiter-at-sysgears/');
-
-    cy.get('.recruiter-headline-lg > a')
-      .then(a => {
-        const compName = a[0]['innerText'].toLowerCase();
-        cy.setCookie('compName', compName);
-      });
-
-    cy.get('.col-sm-8 > :nth-child(1)')
-      .find('.light-button')
-      .then(btn => {
-        const expJobsCount = btn[0]['innerText'].split('(')[1][0];
-        const companyJobsUrl = btn[0]['href'];
-
-        cy.setCookie('expJobsC', expJobsCount);
-        cy.setCookie('compJobsUrl', companyJobsUrl);
-
-        cy.visit(companyJobsUrl);
-      });
-  })
+    cy.visit(Cypress.env('CompanyJobsPage'));
+  });
 
   beforeEach(() => {
-    Cypress.Cookies.preserveOnce(
-      'sessionid', 
-      'compJobsUrl', 
-      'expJobsC', 
-      'compName'
-    );
-    cy.get('.jobs-list-wrapper')
-      .as('jobs-wrapper');
+    const expCompName = Cypress.env('CompanyName').toLowerCase();
+    cy.wrap(expCompName).as('expCompName');
   })
 
-  // after(() => {
-  //   cy.logout();
-  // })
-
-  it('check if jobs count is equal to expected', () => {
-    cy.getCookie('expJobsC').then(countCookie => {
-      const expJobsCount = countCookie.value;
-      cy.get('@jobs-wrapper')
-        .children('div')
-        .should('have.length', expJobsCount)
-    });
-  })
+  it('validate header', () => {
+    cy.get('h1.svelte-1sqbta2').then(title => {
+      const titleLower = title[0]['innerText'].toLowerCase()
+      cy.get('@expCompName').then(name => {
+        expect(titleLower).to.include(name);
+      });
+    })
+  });
 
   it('check if all jobs belongs to expected company (without scroll)', () => {
-    cy.getCookie('compName').then(compNameCookie => {
-      const expCompName = compNameCookie.value;
-      cy.get('@jobs-wrapper')
-        .children('div').within(() => {
-          cy.get('article > .title > small > .company')
-            .then(span => {
-              const nbsp = String.fromCharCode(160);
-              const factCompName = span[0]['innerText'].split(nbsp)[1].toLowerCase();
-              expect(factCompName).to.be.eq(expCompName);
-            })
-        })
-
-    })
+    cy.get('.jobs-list-wrapper')
+      .children('div').within(() => {
+        cy.get('article > .title > small > .company')
+          .then(span => {
+            const nbsp = String.fromCharCode(160);
+            const factCompName = span[0]['innerText'].split(nbsp)[1].toLowerCase();
+            cy.get('@expCompName').then(name => {
+              expect(factCompName).to.be.eq(name);
+            });
+          });
+      });
   })
-
 })
